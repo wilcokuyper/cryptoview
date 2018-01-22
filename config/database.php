6e5c2439a9d81ab6f3e169493c5cb545015d417a
Wilco Kuyper <wilcokuyper@hotmail.com>
2018-01-22 22:15:11 +0100

    typo

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -30,12 +36,6 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-
-    $url = parse_url(getenv("DATABASE_URL"));
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
 
     'connections' => [
 
