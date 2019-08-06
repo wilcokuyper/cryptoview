7a6c3d416f8d1005d66c59a5c05eee41028807e4
Wilco Kuyper <wilcokuyper@hotmail.com>
2019-08-06 21:10:33 +0200

    Fix error with missing postgres config when running locally

--- config/database.php
+++ config/database.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$url = parse_url(getenv('DATABASE_URL'));
+$host = isset($url["host"]) ? $url['host'] : null;
+$username = isset($url["user"]) ? $url['user'] : null;
+$password = isset($url["pass"]) ? $url['pass'] : null;
+$database = isset($url['path']) ? substr($url['path'], 1) : null;
 
 return [
 
