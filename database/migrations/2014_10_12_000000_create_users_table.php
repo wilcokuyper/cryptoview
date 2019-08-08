71088acb8242ba1327973f804807028960d0dfc1
Wilco Kuyper <wilcokuyper@hotmail.com>
2019-08-08 22:12:31 +0200

    Make name field in users table nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
-            $table->string('avatar')->nullable(true);
+            $table->string('avatar')->nullable();
 
             $table->rememberToken();
             $table->timestamps();
