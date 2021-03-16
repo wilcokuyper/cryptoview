6264731bae75203cc7a6260a959f2743a4ebad36
Wilco Kuyper <wilcokuyper@hotmail.com>
2021-03-16 20:35:21 +0100

    Update login route

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('login/{provider}', [LoginController::class,@'login']);
+Route::get('login/{provider}', [LoginController::class, 'login']);
 Route::get('login/{provider}/callback', [LoginController::class, 'login']);
-Route::get('logout', [LoginController::class,'logout']);
+Route::get('logout', [LoginController::class, 'logout']);
 
 Route::view('/{path?}', 'index')
-      ->where('path', '.*')
-      ->name('react');
+    ->where('path', '.*')
+    ->name('react');
 
 Auth::routes();
