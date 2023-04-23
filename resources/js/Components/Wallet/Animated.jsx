3d2b51798895bae8ee49d26b8d586cbf3a805741
wilcokuyper <wilcokuyper@hotmail.com>
2023-04-23 21:52:16 +0200

    Update Animated component to use heroicons instead of font-awesome

--- resources/js/Components/Wallet/Animated.jsx
+++ resources/js/Components/Wallet/Animated.jsx
@@ -1,29 +1,53 @@
 import PropTypes from 'prop-types'
 import React, { useState, useEffect, useRef } from 'react'
 import './Animated.css'
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  MinusIcon,
+} from '@heroicons/react/20/solid'
+import classNames from 'classnames'
 
 function Animated({ children, value }) {
-  const [containerClass, setContainerClass] = useState('')
+  const [status, setStatus] = useState('')
 
   const prevCountRef = useRef()
+
   useEffect(() => {
     if (value > prevCountRef.current) {
-      setContainerClass('price-up animated')
+      setStatus('increase')
     } else if (value < prevCountRef.current) {
-      setContainerClass('price-down animated')
+      setStatus('decrease')
     } else {
-      setContainerClass('')
+      setStatus('')
     }
 
     prevCountRef.current = value
   }, [value])
 
-  return <div className={containerClass}>{children}</div>
+  return (
+    <div
+      className={classNames(
+        'd-flex align-middle justify-content-end animated',
+        {
+          'price-up': status === 'increase',
+          'price-down': status === 'decrease',
+        },
+      )}
+    >
+      <div>{children}</div>
+      <div>
+        {status === 'increase' && <ChevronUpIcon style={{ height: 20 }} />}
+        {status === 'decrease' && <ChevronDownIcon style={{ height: 20 }} />}
+        {status === '' && <MinusIcon style={{ height: 20 }} />}
+      </div>
+    </div>
+  )
 }
 
 Animated.propTypes = {
   value: PropTypes.number,
-  children: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 }
 
 export default Animated
