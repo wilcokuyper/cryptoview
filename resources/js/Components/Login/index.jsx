950d6d8f9a1a4e4c66e90ec67e7c25a204c3b175
wilcokuyper <wilcokuyper@hotmail.com>
2025-03-09 21:53:51 +0100

    Fix login button

--- resources/js/Components/Login/index.jsx
+++ resources/js/Components/Login/index.jsx
@@ -7,7 +7,12 @@ const Login = () => {
         <div className="container">
             <h2>Login</h2>
             <p>Please login with your Facebook account</p>
-            <a href="/login/facebook" className="btn btn-facebook text-white" role="button"><FaFacebook aria-hidden="true"/> Sign in using Facebook</a>
+            <a href="/login/facebook" className="btn btn-facebook text-white" role="button">
+                <div className="d-flex align-items-center">
+                    <FaFacebook aria-hidden="true" className="mr-2"/>
+                    <span>Sign in using Facebook</span>
+                </div>
+            </a>
         </div>
     );
 };
