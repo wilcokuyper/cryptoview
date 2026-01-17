d4b62b22492fc886aa3c9f34ac81e151647e6e64
wilcokuyper <wilcokuyper@hotmail.com>
2026-01-17 18:14:47 +0100

    Fix incorrect pgsql database config

--- config/database.php
+++ config/database.php
@@ -84,7 +84,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -93,7 +93,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => 'cryptoview',
             'sslmode' => 'prefer',
         ],
 
