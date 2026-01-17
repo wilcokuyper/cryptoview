d565326fa84eb8f8c23397dd887a301ac00e22e1
wilcokuyper <wilcokuyper@hotmail.com>
2026-01-17 19:01:53 +0100

    update page title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Cryptoview') }}</title>
+    <title>{{ config('app.name', 'Cryptoview') }} - Manage your crypto assets</title>
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <script>
         (function() {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Cryptoview') }} - Manage your crypto assets</title>
 
     <!-- Favicon -->
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
