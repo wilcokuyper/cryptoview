69757eeddd8eeff8ea5b0bca9ab5d28a0e23d4d0
wilcokuyper <wilcokuyper@hotmail.com>
2026-01-17 19:09:23 +0100

    update favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Cryptoview') }} - Manage your crypto assets</title>
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Cryptoview" />
+    <link rel="manifest" href="/site.webmanifest" />
     <script>
         (function() {
             try {
